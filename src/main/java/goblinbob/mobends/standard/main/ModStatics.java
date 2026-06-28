238b53e0555311ebce17fa31dc46ecd7d514dc92
Nulled <122193939+titanoset@users.noreply.github.com>
2026-06-28 15:20:46 -0400

    Rename mod from Mo' Bends to JustWalk; update logo and resource paths; remove unused animation files and localization resources for a streamlined codebase.

--- src/main/java/goblinbob/mobends/standard/main/ModStatics.java
+++ src/main/java/goblinbob/mobends/standard/main/ModStatics.java
@@ -2,7 +2,7 @@ package goblinbob.mobends.standard.main;
 
 public class ModStatics
 {
-    public static final String MODID = "mobends";
+    public static final String MODID = "justwalk";
     public static final String LOADER = "forge";
     public static final String VERSION = "2.0.0";
     public static final String MCVERSION = "1.20.1";
